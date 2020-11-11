a8b0bfd4742d2b5c02a772e1d7733db90d622ac4
Bayley Evason <bayleyevason@gmail.com>
2020-11-11 15:46:12 +0000

    new player model and animations

---
+++ Lighting game/Assets/animatioStateController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatioStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash = Animator.StringToHash("isWalkingHash");
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        bool isWalking = animator.GetBool("isWalkingHash");
+        bool forwardPressed = Input.GetKey("w");
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool("isWalkingHash", true);
+        }
+        if(isWalking && !forwardPressed)
+        {
+            animator.SetBool("isWalkingHash", false);
+        }
+    }
+}
