38267f6f2782b335f8accf5d8de93e4792384ab7
Bayley Evason <bayleyevason@gmail.com>
2020-11-25 16:13:09 +0000

    item collection and door

---
+++ Lighting game/Assets/Materials/ItemDoor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDoor : MonoBehaviour
+{
+
+    bool canWin = false;
+    public Text NoGems,Interact;
+
+    public GameObject Door;
+    private void Start()
+    {
+        NoGems.enabled = false;
+        Interact.enabled = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canWin = (FindObjectsOfType<ScoringSystem>().Length == 0);
+
+        Interact.enabled = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Interact.enabled = false;
+            if (canWin) 
+            {
+                Destroy(Door);
+                Destroy(gameObject);
+            }
+            else { NoGems.enabled = true; }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        NoGems.enabled = false;
+        Interact.enabled = false;
+    }
+
+
+}
---
+++ Lighting game/Assets/Materials/ScoringSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+  public class ScoringSystem : MonoBehaviour
+{
+    public GameObject ScoreText;
+    public static int theScore;
+    void OnTriggerEnter(Collider other)
+    {
+        theScore += 1;
+        ScoreText.GetComponent<Text>().text = "GEMS Collected " + theScore;
+        Destroy(gameObject);
+    }  
+}
